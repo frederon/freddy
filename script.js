44df78aeea33ca7b37a15db96de853393c350a35
Frederic Ronaldi <frederic.ronaldi@gmail.com>
2020-08-10 12:25:45 +0700

    Add gelonggongan, ikan calculations

--- script.js
+++ script.js
@@ -6,19 +6,28 @@ let indexResult = 1;
 submitButton.on('click', e => {
   e.preventDefault();
   if (formElement.hasClass("form__dagingayam")) {
-    let score = hitungSkor(['kelainan', 'ketebalan', 'lemak', 'utuh', 'memar', 'bersih'], 100);
-    addNewHistory(score)
+    let score = hitungSkorFreshness(['kelainan', 'ketebalan', 'lemak', 'utuh', 'memar', 'bersih'], 100);
+    tambahHasilFreshness(score)
   } else if (formElement.hasClass("form__dagingsapi")) {
-    let score = hitungSkor(['tebal_lemak', 'bentuk', 'warna', 'warna_lemak', 'perubahan_warna', 'tekstur'], 100);
-    addNewHistory(score)
+    let score = hitungSkorFreshness(['tebal_lemak', 'bentuk', 'warna', 'warna_lemak', 'perubahan_warna', 'tekstur'], 100);
+    tambahHasilFreshness(score)
+  } else if (formElement.hasClass("form__dagingikan")) {
+    let score = hitungSkorIkan(['bolamata', 'korpup', 'mata', 'warna', 'lendir', 'warna-lendir', 'tekstur', 'kilapan', 'sayatan', 'jaringan', 'bau', 'kepadatan', 'elastisitas']);
+    tambahHasilFreshness(score / 54 * 100)
   } else if (formElement.hasClass("form__sayur")) {
-    let score = hitungSkor(['amis', 'lendir', 'gelap', 'lembek', 'pucuk'], 100)
-    addNewHistory(score)
+    let score = hitungSkorFreshness(['amis', 'lendir', 'gelap', 'lembek', 'pucuk'], 100)
+    tambahHasilFreshness(score)
+  } else if (formElement.hasClass("form__ayamtiren")) {
+    let res = hitungKeamanan(['daging-kuning', 'daging-gelap', 'bercak', 'kebiruan', 'lembek', 'organ-gelap', 'amis', 'nadi'])
+    tambahHasilKeamanan(res);
+  } else if (formElement.hasClass("form__gelonggongan")) {
+    let res = hitungKeamanan(['kepucatan', 'tekstur', 'berair', 'harga'])
+    tambahHasilKeamanan(res);
   }
   indexResult += 1;
 })
 
-function hitungSkor(arrayNama, max) {
+function hitungSkorFreshness(arrayNama, max) {
   let score = 0;
   let count = arrayNama.filter(el => $(`input[name=${el}]:checked`).val() && $(`input[name=${el}]:checked`).val() !== "lewati").length;
   arrayNama.forEach(element => {
@@ -34,7 +43,7 @@ function hitungSkor(arrayNama, max) {
   return score;
 }
 
-function addNewHistory(score) {
+function tambahHasilFreshness(score) {
   $('.container').append(`
     <div class="soalcek result" id="result${indexResult}">
       <h1>Hasil :</h1>
@@ -47,4 +56,43 @@ function addNewHistory(score) {
   $('html, body').animate({
     scrollTop: $(`#result${indexResult}`).offset().top
   }, 100);
+}
+
+function hitungKeamanan(arrayNama) {
+  arrayNama = arrayNama.filter(el => $(`input[name=${el}]:checked`).val() && $(`input[name=${el}]:checked`).val() !== "lewati")
+  if (arrayNama.every(val => $(`input[name=${val}]:checked`).val() === "ya")) {
+    return 0;
+  } else if (arrayNama.every(val => $(`input[name=${val}]:checked`).val() === "tidak")) {
+    return 2;
+  }
+  return 1;
+}
+
+function tambahHasilKeamanan(res) {
+  $('.container').append(`
+    <div class="soalcek result" id="result${indexResult}">
+      <h1>Hasil :</h1>
+      <span>Dihitung pada ${new Date()}</span>
+      <h2 class="${res === 0 ? 'red' : res === 2 ? 'green' : ''}">
+        ${$('form').attr('class').includes('tiren') ? 
+          res === 0 ? 'Ayam tiren' : res === 2 ? 'Bukan ayam tiren' : 'Mungkin ayam tiren'
+        :
+          res === 0 ? 'Gelonggongan' : res === 2 ? 'Bukan gelonggongan' : 'Mungkin gelonggongan'
+        }
+      </h2>
+    </div>
+  `)
+  $('html, body').animate({
+    scrollTop: $(`#result${indexResult}`).offset().top
+  }, 100);
+}
+
+function hitungSkorIkan(arrayNama) {
+  let score = 0;
+  arrayNama = arrayNama.filter(el => $(`input[name=${el}]:checked`).val() && $(`input[name=${el}]:checked`).val() !== "lewati");
+  arrayNama.forEach(element => {
+    const val = Number($(`input[name=${element}]:checked`).val());
+    score += val;
+  });
+  return score;
 }
